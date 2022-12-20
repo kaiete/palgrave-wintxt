bc46745697f48cd2ee454d9538f00bbf6e3f1d76
kaiete <kaiete@aragon-press.com>
2022-12-20 16:59:29 +0000

    fix my errors
    to err is human
    I have made a lot of mistakes in my time

--- palgrave-wintxt.cpp
+++ palgrave-wintxt.cpp
@@ -7,20 +7,21 @@ int main()
     cout << "< Hello!\n(press any key)\n";
     int getchnum;
     getchnum = _getch();
+    string favtext = "My favourite number is ";
+    string favnum = favtext + to_string(getchnum);
+    cout << favnum;
+    cout << "\n";
     string textin;
     while (1) {
         cout << "> ";
         cin >> textin;
-        if (textin == "hello") {
+        if (textin == "hello")
+        {
             cout << "Hello to you too!";
         }
-        if (textin == "exit") {
+        if (textin == "exit")
+        {
             exit(1);
-        }
-        if (textin.find("number")) {
-            string favtext = "My favourite number is ";
-            string favnum = favtext + to_string(getchnum);
-            cout << favnum;
         }
         cout << "\n";
     }
