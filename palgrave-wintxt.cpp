32988b7f62456e1fad7f43d6f0816430f2bc3577
kaiete <kaiete@aragon-press.com>
2022-12-20 16:05:04 +0000

    Add project files.

---
+++ palgrave-wintxt.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "< Hello!\n(press any key)\n";
+    int getchnum;
+    getchnum = _getch();
+    cout << "debug: ";
+    cout << getchnum;
+    string textin;
+    while (1) {
+        cout << "> ";
+        cin >> textin;
+        if (textin == "hello") {
+            cout << "Hello to you too!";
+        }
+        if (textin == "exit") {
+            exit(1);
+        }
+        if (textin.find("number")) {
+            string favtext = "My favourite number is ";
+            string favnum = favtext + to_string(getchnum);
+            cout << favnum;
+        }
+        cout << "\n";
+    }
+}
